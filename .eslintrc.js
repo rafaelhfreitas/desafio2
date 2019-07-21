c382390f691b831cba3534d9b7ae820618549c6d
Rafael <oraffaudi@gmail.com>
2019-07-21 12:38:48 -0300

    Configuration sequelize, eslint, nodemon, sucrase, migration User

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
     env: {
         es6: true,
-        node: true
+        node: true,
     },
     extends: ['airbnb-base', 'prettier'],
     plugins: ['prettier'],
     globals: {
         Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly'
+        SharedArrayBuffer: 'readonly',
     },
     parserOptions: {
         ecmaVersion: 2018,
@@ -17,7 +17,7 @@ module.exports = {
             'class-methods-use-this': 'off',
             'no-param-reassign': 'off',
             camelcase: 'off',
-            'no-unused-vars': ['error', { argsIgnorePattern: 'next' }]
-        }
-    }
+            'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+        },
+    },
 };
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
 
+import User from './app/models/Users';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-    return res.json({ message: 'Hello World' });
+routes.get('/', async (req, res) => {
+    const user = await User.create({
+        name: 'Rafael Freitas',
+        email: 'rafael@bigworks.com',
+        password_hash: '12345',
+    });
+    return res.json(user);
 });
 
 export default routes;
